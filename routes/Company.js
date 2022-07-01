6ce0a4cc08d1d380660f4f899614cc6d4aa3f7b0
Đỗ Thanh Bình <binhdothanh.navn@gmail.com>
2022-07-01 21:21:26 +0700

    test cors

--- routes/Company.js
+++ routes/Company.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
   var Company = require('../controller/Company');
   var router = require('express').Router();
-
+  var cors = require('cors');
   router.post('/', Company.create);
-  router.get('/', Company.findall);
+  router.get('/', Company.findall,cors());
   router.get('/search', Company.search);
   router.get('/:id', Company.findone);
   router.delete('/:id', Company.delete);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
 
-app.use(cors());
+app.use('*', cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
